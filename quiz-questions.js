cf3ba231fcb4faaec8e65ec122658e2fd663f652
kumarshubham-ksh <kumarshubham.ksh@gmail.com>
2024-07-23 22:14:56 +0530

    first commit

---
+++ quiz-questions.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+        num: 1,
+        question: "Where is Sanchi Stupa located?",
+        answer: "Madhya Pradesh",
+        options: [
+            "Odisha",
+            "Chhattisgarh",
+            "Madhya Pradesh",
+            "Uttar Pradesh"
+        ]
+    },
+    {
+        num: 2,
+        question: "Where is Qutub Minar located?",
+        answer: "Delhi",
+        options: [
+            "Delhi",
+            "Haryana",
+            "Uttar Pradesh",
+            "Rajasthan"
+        ]
+    },
+    {
+        num: 3,
+        question: "Where is Mysore Palace located?",
+        answer: "Karnataka",
+        options: [
+            "Telangana",
+            "Kerala",
+            "Karnataka",
+            "Tamil Nadu"
+        ]
+    },
+    {
+        num: 4,
+        question: "Where is Fatehpur Sikri located?",
+        answer: "Uttar Pradesh",
+        options: [
+            "Uttar Pradesh",
+            "Haryana",
+            "Delhi",
+            "Madhya Pradesh"
+        ]
+    },
+    {
+        num: 5,
+        question: "Where is India Gate located?",
+        answer: "Delhi",
+        options: [
+            "Uttar Pradesh",
+            "Punjab",
+            "Haryana",
+            "Delhi"
+        ]
+    },
+    {
+        num: 6,
+        question: "Where is Char Minar located?",
+        answer: "Telangana",
+        options: [
+            "Maharashtra",
+            "Andhra Pradesh",
+            "Karnataka",
+            "Telangana"
+        ]
+    },
+    {
+        num: 7,
+        question: "Where are Elephanta Caves located?",
+        answer: "Maharashtra",
+        options: [
+            "Madhya Pradesh",
+            "Maharashtra",
+            "Karnataka",
+            "Gujarat"
+        ]
+    },
+    {
+        num: 8,
+        question: "Where is Konark Sun Temple located?",
+        answer: "Odisha",
+        options: [
+            "Chhattisgarh",
+            "Jharkhand",
+            "West Bengal",
+            "Odisha"
+        ]
+    },
+    {
+        num: 9,
+        question: "Where is Hawa Mahal located?",
+        answer: "Rajasthan",
+        options: [
+            "Gujarat",
+            "Rajasthan",
+            "Madhya Pradesh",
+            "Punjab"
+        ]
+    },
+    {
+        num: 10,
+        question: "Where are Ajanta and Ellora Caves located?",
+        answer: "Maharashtra",
+        options: [
+            "Gujarat",
+            "Telangana",
+            "Maharashtra",
+            "Madhya Pradesh"
+        ]
+    }
+]
